bd96ae4e7265a880f835882e0338798ed25b51cc
Quentin-Desmettre <quentin.desmettre@epitech.eu>
2023-05-08 15:16:50 +0200

    feat(forms): Implemented torus/tanglecube

--- include/objects/Torus.hpp
+++ include/objects/Torus.hpp
@@ -13,7 +13,7 @@
 class Torus: public AObject {
 public:
     bool intersect(unused const Ray &ray, Vec3 &intersection) const override;
-    sf::Vector3f getNormal(unused const Vec3 &inter, unused const Ray &ray) const override {return VEC3_ZERO;}
+    sf::Vector3f getNormal(unused const Vec3 &inter, unused const Ray &ray) const override;
 
     // Setters
     void setRadius(const float &radius);
--- src/objects/Torus.cpp
+++ src/objects/Torus.cpp
@@ -24,14 +24,59 @@ void Torus::setThickness(const float &thickness)
 
 bool Torus::intersect(const Ray &ray, Vec3 &intersection) const
 {
-    // Formula of a torus:
-    // (x^2 + y^2 + z^2 + R^2 - r^2)^2 - 4R^2(x^2 + y^2) = 0
-    // Parametric equation of a line:
-    // x = x0 + t * dx
-    // y = y0 + t * dy
-    // z = z0 + t * dz
+    Ray r = transformRay(ray);
+    float
+        x = r.getOrigin().x,
+        y = r.getOrigin().y,
+        z = r.getOrigin().z,
+        dx = r.getDir().x,
+        dy = r.getDir().y,
+        dz = r.getDir().z;
+    float sum_d_sqrd = dx * dx + dy * dy + dz * dz;
+    float e = x * x + y * y + z * z - _radius * _radius - _thickness * _thickness;
+    float f = x * dx + y * dy + z * dz;
+    float four_a_sqrd = 4.0f * _radius * _radius;
 
-    // So, intersection between a line and a torus is:
-    // (x0 + t * dx)^2 + (y0 + t * dy)^2 + (z0 + t * dz)^2 + R^2 - r^2)^2 - 4R^2((x0 + t * dx)^2 + (y0 + t * dy)^2) = 0
+    float coeffs[5] = {
+            sum_d_sqrd * sum_d_sqrd,
+            4.0f * sum_d_sqrd * f,
+            2.0f * sum_d_sqrd * e + 4.0f * f * f + four_a_sqrd * dy * dy,
+            4.0f * f * e + 2.0f * four_a_sqrd * y * dy,
+            e * e - four_a_sqrd * (_thickness * _thickness - y * y)
+    };
 
+    auto solutions = Math::solveQuartic(coeffs);
+    if (solutions.empty())
+        return false;
+    float t = INFINITY;
+    bool found = false;
+    for (auto solution : solutions) {
+        if (solution > 0 && solution < t) {
+            t = solution;
+            found = true;
+        }
+    }
+    if (!found)
+        return false;
+    intersection = _transformationsMatrix * (r.getOrigin() + r.getDir() * t);
+    return true;
+}
+
+Vec3 Torus::getNormal(const Vec3 &inter, unused const Ray &ray) const
+{
+    // Put intersection in local space
+    Vec3 intersection = _inverseTransformations * inter;
+    float param_squared = _radius * _radius + _thickness * _thickness;
+
+    float
+        x = intersection.x,
+        y = intersection.y,
+        z = intersection.z;
+    float sum_squared = x * x + y * y + z * z;
+
+    return Math::normalize({
+           4.0f * x * (sum_squared - param_squared),
+           4.0f * y * (sum_squared - param_squared + 2.0f * _radius * _radius),
+           4.0f * z * (sum_squared - param_squared)
+   });
 }
