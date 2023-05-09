077e898891f76e67f790d0b1d9adcffb2589e5ce
Edo <edgar.lecuyer@epitech.eu>
2023-05-09 17:33:32 +0200

    fix: fixed obj for main

--- src/objects/Triangle.cpp
+++ src/objects/Triangle.cpp
@@ -4,9 +4,7 @@
 ** File description:
 ** Triangle
 */
-
 #include "objects/Triangle.hpp"
-
 Triangle::Triangle(Vec3 point1, Vec3 point2, Vec3 point3,
 sf::Color color, sf::Color emmsionColor, float intensity) :
 AObject(point1, color, emmsionColor, intensity)
@@ -18,17 +16,17 @@ AObject(point1, color, emmsionColor, intensity)
 
 bool Triangle::intersect(const Ray &ray, Vec3 &intersection) const
 {
-    Ray r = transformRay(ray);
-    Vec3 origin = r.getOrigin();
-    Vec3 dir = r.getDir();
-    Vec3 edge1 = _points[1] - _points[0];
-    Vec3 edge2 = _points[2] - _points[0];
+    std::array<Vec3, 3> points = getTmpPoints(ray);
+    Vec3 origin = ray.getOrigin();
+    Vec3 dir = ray.getDir();
+    Vec3 edge1 = points[1] - points[0];
+    Vec3 edge2 = points[2] - points[0];
     Vec3 pvec = Math::cross(dir, edge2);
     float det = Math::dot(edge1, pvec);
 
     if (det < 0.0001f)
         return false;
-    Vec3 tvec = origin - _points[0];
+    Vec3 tvec = origin - points[0];
     float u = Math::dot(tvec, pvec);
     if (u < 0.0f || u > det)
         return false;
@@ -40,44 +38,38 @@ bool Triangle::intersect(const Ray &ray, Vec3 &intersection) const
     if (t < 0.0f)
         return false;
     intersection = origin + dir * t;
-    intersection = _transformationsMatrix * intersection;
     return true;
 }
 
-//    std::array<Vec3, 3> points = getTmpPoints(ray);
-//    Vec3 origin = ray.getOrigin();
-//    Vec3 dir = ray.getDir();
-//    Vec3 edge1 = points[1] - points[0];
-//    Vec3 edge2 = points[2] - points[0];
+// Vec3 Triangle::getIntersection(const Ray &ray) const
+// {
+//     Vec3 origin = ray.getOrigin();
+//     Vec3 dir = ray.getDir();
+//     Vec3 edge1 = _points[1] - _points[0];
+//     Vec3 edge2 = _points[2] - _points[0];
+//     Vec3 pvec = Math::cross(dir, edge2);
+//     float det = Math::dot(edge1, pvec);
 
-//Vec3 Triangle::getIntersection(const Ray &ray) const
-//{
-//    Vec3 origin = ray.getOrigin();
-//    Vec3 dir = ray.getDir();
-//    Vec3 edge1 = _points[1] - _points[0];
-//    Vec3 edge2 = _points[2] - _points[0];
-//    Vec3 pvec = Math::cross(dir, edge2);
-//    float det = Math::dot(edge1, pvec);
+//     Vec3 tvec = origin - _points[0];
+//     Vec3 qvec = Math::cross(tvec, edge1);
+//     float t = Math::dot(edge2, qvec) / det;
+//     return origin + dir * t;
+// }
 
-//    Vec3 tvec = origin - _points[0];
-//    Vec3 qvec = Math::cross(tvec, edge1);
-//    float t = Math::dot(edge2, qvec) / det;
-//    return origin + dir * t;
-//}
 
-//std::array<Vec3, 3> Triangle::getTmpPoints(const Ray &ray) const
-//{
-//    Vec3 edge1 = _points[1] - _points[0];
-//    Vec3 edge2 = _points[2] - _points[0];
-//    Vec3 normal = Math::normalize(Math::cross(edge1, edge2));
-//    float dir = Math::dot(normal, ray.getDir());
-    
-//    if (dir < 0) {
-//        return _points;
-//    } else {
-//        return std::array<Vec3, 3>{_points[2], _points[1], _points[0]};
-//    }
-//}
+std::array<Vec3, 3> Triangle::getTmpPoints(const Ray &ray) const
+{
+    Vec3 edge1 = _points[1] - _points[0];
+    Vec3 edge2 = _points[2] - _points[0];
+    Vec3 normal = Math::normalize(Math::cross(edge1, edge2));
+    float dir = Math::dot(normal, ray.getDir());
+
+    if (dir < 0) {
+        return _points;
+    } else {
+        return std::array<Vec3, 3>{_points[2], _points[1], _points[0]};
+    }
+}
 
 Vec3 Triangle::getNormal(unused const Vec3 &inter, const Ray &ray) const
 {
